f7c6ddb0f5d9ca21b6549095a11df4044bd5e85c
Aaron Cremona <dubbacremona@gmail.com>
2015-12-06 15:12:16 -0500

    added more quotes and embedded spotify player with album art

--- random-quote/quotes.js
+++ random-quote/quotes.js
@@ -8,7 +8,9 @@ var quoteMachine = {
 				And then one day you find <br> \
 				Ten years have got behind you <br> \
 				No one told you when to run <br> \
-				You missed the starting gun"
+				You missed the starting gun",
+			uri: "spotify:track:23W5DHu31ZLW9q0p2wQxfN",
+			embedCode: '<iframe src="https://embed.spotify.com/?uri=spotify%3Atrack%3A23W5DHu31ZLW9q0p2wQxfN" width="300" height="380" frameborder="0" allowtransparency="true"></iframe>'
 		},
 		"2": {
 			artist: "Bob Dylan",
@@ -16,18 +18,24 @@ var quoteMachine = {
 			quote: "Yes, how many times can a man turn his head <br> \
 				Pretending he just doesn't see ? <br> \
 				The answer my friend is blowin' in the wind <br> \
-				The answer is blowin' in the wind." 
+				The answer is blowin' in the wind.",
+			uri: "spotify:track:18GiV1BaXzPVYpp9rmOg0E",
+			embedCode: '<iframe src="https://embed.spotify.com/?uri=spotify%3Atrack%3A18GiV1BaXzPVYpp9rmOg0E" width="300" height="380" frameborder="0" allowtransparency="true"></iframe>'
 		},
 		"3": {
 			artist: "Billy Joel",
 			songTitle: "Piano Man",
 			quote: "Yes, they're sharing a drink they call loneliness <br> \
-				But it's better than drinkin' alone"
+				But it's better than drinkin' alone",
+			uri: "spotify:track:70C4NyhjD5OZUMzvWZ3njJ",
+			embedCode: '<iframe src="https://embed.spotify.com/?uri=spotify%3Atrack%3A70C4NyhjD5OZUMzvWZ3njJ" width="300" height="380" frameborder="0" allowtransparency="true"></iframe>'
 		},
 		"4": {
 			artist: "John Lennon",
 			songTitle: "Imagine",
-			quote: "No hell below us, above us only skies, Imagine all the people living for today"
+			quote: "No hell below us, above us only skies, Imagine all the people living for today",
+			uri: "spotify:track:7pKfPomDEeI4TPT6EOYjn9",
+			embedCode: '<iframe src="https://embed.spotify.com/?uri=spotify%3Atrack%3A7pKfPomDEeI4TPT6EOYjn9" width="300" height="380" frameborder="0" allowtransparency="true"></iframe>'
 		},
 		"5": {
 			artist: "Pink Floyd",
@@ -35,12 +43,16 @@ var quoteMachine = {
 			quote: "Running over the same old ground. <br> \
 				What have we found <br> \
 				The same old fears. <br> \
-				Wish you were here." 
+				Wish you were here." ,
+			uri: "spotify:track:1wHSxWBfPr3GO31y8KGrWe",
+			embedCode: '<iframe src="https://embed.spotify.com/?uri=spotify%3Atrack%3A1wHSxWBfPr3GO31y8KGrWe" width="300" height="380" frameborder="0" allowtransparency="true"></iframe>'
 		},
 		"6": {
 			artist: "Simon & Garfunkel",
 			songTitle: "Sound of Silence",
-			quote: "People talking without speaking, people hearing without listening"
+			quote: "People talking without speaking, people hearing without listening",
+			uri: "spotify:track:2LkaNhCrNVmcYgXJeLVmsw",
+			embedCode: '<iframe src="https://embed.spotify.com/?uri=spotify%3Atrack%3A2LkaNhCrNVmcYgXJeLVmsw" width="300" height="380" frameborder="0" allowtransparency="true"></iframe>'
 		},
 		"7": {
 			artist: "Bob Dylan",
@@ -53,14 +65,18 @@ var quoteMachine = {
 				And glowed like burning coal <br> \
 				Pouring off of every page <br> \
 				Like it was written in my soul from me to you <br> \
-				Tangled up in blue"
+				Tangled up in blue",
+			uri: "spotify:track:6Vcwr9tb3ZLO63F8DL8cqu",
+			embedCode: '<iframe src="https://embed.spotify.com/?uri=spotify%3Atrack%3A6Vcwr9tb3ZLO63F8DL8cqu" width="300" height="380" frameborder="0" allowtransparency="true"></iframe>'
 		},
 		"8": {
 			artist: "Eminem",
 			songTitle: "Lose Yourself",
 			quote: "Look, if you had, one shot or one opportunity <br> \
 				To seize everything you ever wanted in one moment <br> \
-				Would you capture it, or just let it slip?" 
+				Would you capture it, or just let it slip?",
+			uri: "spotify:track:5Z01UMMf7V1o0MzF86s6WJ",
+			embedCode: '<iframe src="https://embed.spotify.com/?uri=spotify%3Atrack%3A5Z01UMMf7V1o0MzF86s6WJ" width="300" height="380" frameborder="0" allowtransparency="true"></iframe>'
 		},
 		"9": {
 			artist: "Bob Dylan",
@@ -69,84 +85,242 @@ var quoteMachine = {
 				With the mystery tramp, but now you realize <br> \
 				He’s not selling any alibis <br> \
 				As you stare into the vacuum of his eyes <br> \
-				And ask him do you want to make a deal?"
+				And ask him do you want to make a deal?",
+			uri: "spotify:track:3AhXZa8sUQht0UEdBJgpGc",
+			embedCode: '<iframe src="https://embed.spotify.com/?uri=spotify%3Atrack%3A3AhXZa8sUQht0UEdBJgpGc" width="300" height="380" frameborder="0" allowtransparency="true"></iframe>'
 		},
 		"10": {
 			artist: "Avicii",
 			songTitle: "Wake Me Up",
 			quote: "I can't tell where the journey will end <br> \
-				But I know where to start"
+				But I know where to start",
+			uri: "spotify:track:4h8VwCb1MTGoLKueQ1WgbD",
+			embedCode: '<iframe src="https://embed.spotify.com/?uri=spotify%3Atrack%3A4h8VwCb1MTGoLKueQ1WgbD" width="300" height="380" frameborder="0" allowtransparency="true"></iframe>'
 		},
 		"11": {
-			artist: "",
-			songTitle: "",
-			quote: ""
+			artist: "Muse",
+			songTitle: "Falling Away With You",
+			quote: "So I'll love whatever you become \
+				And forget the reckless things we've done \
+				I think our lives have just begun \
+				I think our lives have just begun",
+			uri: "spotify:track:5cvFycFL4BTMA3dHSstMqi",
+			embedCode: 'iframe src="https://embed.spotify.com/?uri=spotify%3Atrack%3A5cvFycFL4BTMA3dHSstMqi" width="300" height="380" frameborder="0" allowtransparency="true"></iframe>'
 		},
-		/*
+		"12": {
+			artist: "Primitive Radio Gods",
+			songTitle: "Standing Outside a Broken Phone Booth With Money in My Hand",
+			quote: "We sit outside and argue all night long \
+				About a god we've never seen \
+				But never fails to side with me",
+			uri: "spotify:track:5hcer4MP5GeHqtSjDYx5AC",
+			embedCode: '<iframe src="https://embed.spotify.com/?uri=spotify%3Atrack%3A5hcer4MP5GeHqtSjDYx5AC" width="300" height="380" frameborder="0" allowtransparency="true"></iframe>'
+		},
+		"13": {
+			artist: "Tracy Chapman",
+			songTitle: "Fast Car",
+			quote: "You and I can both get jobs\
+				And finally see what it means to be living",
+			uri: "spotify:track:2M9ro2krNb7nr7HSprkEgo",
+			embedCode: '<iframe src="https://embed.spotify.com/?uri=spotify%3Atrack%3A2M9ro2krNb7nr7HSprkEgo" width="300" height="380" frameborder="0" allowtransparency="true"></iframe>'
+		},
+		"14": {
+			artist: "Johnny Cash",
+			songTitle: "Hurt",
+			quote: "If I could start again \
+				A million miles away \
+				I will keep myself \
+				I would find a way",
+			uri: "spotify:track:5rpRzNcJZqKQXk9PIjreB6",
+			embedCode: 'spotify:track:5rpRzNcJZqKQXk9PIjreB6'
+		},
+		"15": {
+			artist: "Otis Redding",
+			songTitle: "(Sittin' On) the Dock of a Bay",
+			quote: "Look like nothin's gonna change \
+				Everything, still remains the same \
+				I can't do what ten people tell me to do",
+			uri: "spotify:track:4Y4Gd3ty8uut6Qw43c7yJc",
+			embedCode: '<iframe src="https://embed.spotify.com/?uri=spotify%3Atrack%3A4Y4Gd3ty8uut6Qw43c7yJc" width="300" height="380" frameborder="0" allowtransparency="true"></iframe>'
+		},
+		"16": {
+			artist: "Spoon",
+			songTitle: "The Underdog",
+			quote: "You got no time for the messenger, \
+				Got no regard for the thing that you don't understand, \
+				You got no fear of the underdog, \
+				That's why you will not survive!",
+			uri: "spotify:track:2YRuexk6q6so8F5M69n4t1",
+			embedCode: '<iframe src="https://embed.spotify.com/?uri=spotify%3Atrack%3A2YRuexk6q6so8F5M69n4t1" width="300" height="380" frameborder="0" allowtransparency="true"></iframe>'
+		},
+		"17": {
+			artist: "The Flaming Lips",
+			songTitle: "Do You Realise?",
+			quote: "And instead of saying all of your goodbyes, let them know \
+				You realize that life goes fast \
+				It's hard to make the good things last \
+				You realize the sun doesn't go down \
+				It's just an illusion caused by the world spinning round",
+			uri: "spotify:track:2DFRFqWNahKtFD112H2iEZ",
+			embedCode: '<iframe src="https://embed.spotify.com/?uri=spotify%3Atrack%3A2DFRFqWNahKtFD112H2iEZ" width="300" height="380" frameborder="0" allowtransparency="true"></iframe>'
+		},
+		"18": {
+			artist: "Modest Mouse",
+			songTitle: "3rd Planet",
+			quote: "The third planet is sure they’re being watched by an eye in the sky that can’t be stopped. \
+				When they get to the promise land, they’re gonna shake the eye’s hands.",
+			uri: "spotify:track:6V0g1p7KaOzWc3wbjPlQuX",
+			embedCode: '<iframe src="https://embed.spotify.com/?uri=spotify%3Atrack%3A6V0g1p7KaOzWc3wbjPlQuX" width="300" height="380" frameborder="0" allowtransparency="true"></iframe>'
+		},
+		"19": {
+			artist: "Modest Mouse",
+			songTitle: "Blame it on the Tetons",
+			quote: "Language is the liquid that we’re all dissolved in. \
+				Great for solving problems, after it creates the problems.",
+			uri: "spotify:track:2q5zd5u4A4JzAbes2VDPjk",
+			embedCode: '<iframe src="https://embed.spotify.com/?uri=spotify%3Atrack%3A2q5zd5u4A4JzAbes2VDPjk" width="300" height="380" frameborder="0" allowtransparency="true"></iframe>'
+		},
+		"20": {
+			artist: "Portugal the Man",
+			songTitle: "Modern Jesus",
+			quote: "And when the wave approaches \
+				Take our ashes to the ocean \
+				Who cares if hell awaits? \
+				We're having drinks at heaven's gate",
+			uri: "spotify:track:7iOyMOxdOULvXqUqC26spJ",
+			embedCode: '<iframe src="https://embed.spotify.com/?uri=spotify%3Atrack%3A6WGaVdhY8uMWMgxiKpdLVy" width="300" height="380" frameborder="0" allowtransparency="true"></iframe>'
+		},
+		"21": {
+			artist: "Tracy Bonham",
+			songTitle: "Mother Mother",
+			quote: "Yeah, I'm working, making money \
+				I'm just starting to build a name \
+				I can feel it around the corner \
+				I could make it anyday",
+			uri: "spotify:track:6WGaVdhY8uMWMgxiKpdLVy",
+			embedCode: '<iframe src="https://embed.spotify.com/?uri=spotify%3Atrack%3A6WGaVdhY8uMWMgxiKpdLVy" width="300" height="380" frameborder="0" allowtransparency="true"></iframe>'
+		},
+		"22": {
+			artist: "Beck",
+			songTitle: "Lost Cause",
+			quote: "I'm tired of fighting \
+				Fighting for a lost cause",
+			uri: "spotify:track:29i8axWOLDmgbiutJb9prB",
+			embedCode: '<iframe src="https://embed.spotify.com/?uri=spotify%3Atrack%3A29i8axWOLDmgbiutJb9prB" width="300" height="380" frameborder="0" allowtransparency="true"></iframe>'
+		},
+		"23": {
+			artist: "Elliott Smith",
+			songTitle: "Happiness",
+			quote: "What I used to be will pass away and then you’ll see \
+				That all I want now is happiness for you and me.",
+			uri: "spotify:track:3aU2ui8JQOtmgHYH0g1qFL",
+			embedCode: '<iframe src="https://embed.spotify.com/?uri=spotify%3Atrack%3A3aU2ui8JQOtmgHYH0g1qFL" width="300" height="380" frameborder="0" allowtransparency="true"></iframe>'
+		},
+		"24": {
+			artist: "Elliott Smith",
+			songTitle: "King's Crossing",
+			quote: "The method acting that pays my bills \
+				Keeps a fat man feeding in Beverly Hills \
+				I've got a heavy metal mouth, it hurls obscenity \
+				And I get my check from the trash treasury \
+				Because I took my own insides out.",
+			uri: "spotify:track:5BtbRb1XfakaECaQoa0AOo",
+			embedCode: '<iframe src="https://embed.spotify.com/?uri=spotify%3Atrack%3A5BtbRb1XfakaECaQoa0AOo" width="300" height="380" frameborder="0" allowtransparency="true"></iframe>'
+		},
+		"25": {
+			artist: "Elliott Smith",
+			songTitle: "A Distorted Reality is Now a Necessity to be Free” Lyrics",
+			quote: "“You disappoint me \
+			You people raking in on the world \
+				The devil’s script sells you the heart of a black bird \
+				Shine on me baby \
+				Cause it’s raining in my heart.”",
+			uri: "spotify:track:3Ak7zpMulNgKOyaseyyJSv",
+			embedCode: '<iframe src="https://embed.spotify.com/?uri=spotify%3Atrack%3A3Ak7zpMulNgKOyaseyyJSv" width="300" height="380" frameborder="0" allowtransparency="true"></iframe>'
+		},
+		"26": {
+			artist: "Smashing Pumpkins",
+			songTitle: "Thirty-Three",
+			quote: "And for a moment I lose myself \
+				Wrapped up in the pleasures of the world \
+				I've journeyed here and there and back again \
+				But in the same old haunts, \
+				I still find my friends",
+			uri: "spotify:track:02BSpb2sKuFgs0Go3LRbTj",
+			embedCode: '<iframe src="https://embed.spotify.com/?uri=spotify%3Atrack%3A02BSpb2sKuFgs0Go3LRbTj" width="300" height="380" frameborder="0" allowtransparency="true"></iframe>'
+		},
+		"27": {
+			artist: "Simon and Garfunkel",
+			songTitle: "The Only Living Boy in New York",
+			quote: "I get the news I need on the weather report. \
+				I can gather all the news I need on the weather report. \
+				Hey, I've got nothing to do today but smile.",
+			uri: "spotify:track:5MbXzXGbqobR8xPVPs8OXA",
+			embedCode: '<iframe src="https://embed.spotify.com/?uri=spotify%3Atrack%3A5MbXzXGbqobR8xPVPs8OXA" width="300" height="380" frameborder="0" allowtransparency="true"></iframe>'
+		} /*
 		: {
 			artist: "",
 			songTitle: "",
-			quote: ""
-		},
+			quote: "",
+			uri: "",
+			embedCode: ''
+		} 
 		: {
 			artist: "",
 			songTitle: "",
-			quote: ""
-		},
+			quote: "",
+			uri: "",
+			embedCode: ''
+		} 
 		: {
 			artist: "",
 			songTitle: "",
-			quote: ""
-		},
+			quote: "",
+			uri: "",
+			embedCode: ''
+		} 
 		: {
 			artist: "",
 			songTitle: "",
-			quote: ""
-		},
+			quote: "",
+			uri: "",
+			embedCode: ''
+		} 
 		: {
 			artist: "",
 			songTitle: "",
-			quote: ""
-		},
+			quote: "",
+			uri: "",
+			embedCode: ''
+		} 
 		: {
 			artist: "",
 			songTitle: "",
-			quote: ""
-		},
+			quote: "",
+			uri: "",
+			embedCode: ''
+		} 
 		: {
 			artist: "",
 			songTitle: "",
-			quote: ""
-		},
+			quote: "",
+			uri: "",
+			embedCode: ''
+		} 
 		: {
 			artist: "",
 			songTitle: "",
-			quote: ""
-		},
-		: {
-			artist: "",
-			songTitle: "",
-			quote: ""
-		},
-		: {
-			artist: "",
-			songTitle: "",
-			quote: ""
-		},
-		: {
-			artist: "",
-			songTitle: "",
-			quote: ""
-		},
-		: {
-			artist: "",
-			songTitle: "",
-			quote: ""
-		},
-		: {
-			artist: "",
-			songTitle: "",
-			quote: ""
-		} */
+			quote: "",
+			uri: "",
+			embedCode: ''
+		} 
+		*/
+	},
+
+	countChar: function() {
+		for(key in this.quotes) {
+			console.log("Key # " + key + "is " + this.quotes[key].quote.length + "characters");
+		}
 	}
 }
+
