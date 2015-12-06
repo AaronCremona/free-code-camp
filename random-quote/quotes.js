ea065765a018a8d3f37f49ab7dc1a9e56cb2eb22
unknown <aaron.cremona@GEDULAP-AC.EDUPRIZE.LOCAL>
2015-12-06 00:05:27 -0700

    added song quote content

---
+++ random-quote/quotes.js
@@ -0,0 +1,152 @@
+var quoteMachine = {
+	quotes: {
+		"1": {
+			artist: "Pink Floyd",
+			songTitle: "Time",
+			quote: "You are young and life is long <br> \
+				and there is time to kill today <br> \
+				And then one day you find <br> \
+				Ten years have got behind you <br> \
+				No one told you when to run <br> \
+				You missed the starting gun"
+		},
+		"2": {
+			artist: "Bob Dylan",
+			songTitle: "Blowin' in the Wind",
+			quote: "Yes, how many times can a man turn his head <br> \
+				Pretending he just doesn't see ? <br> \
+				The answer my friend is blowin' in the wind <br> \
+				The answer is blowin' in the wind." 
+		},
+		"3": {
+			artist: "Billy Joel",
+			songTitle: "Piano Man",
+			quote: "Yes, they're sharing a drink they call loneliness <br> \
+				But it's better than drinkin' alone"
+		},
+		"4": {
+			artist: "John Lennon",
+			songTitle: "Imagine",
+			quote: "No hell below us, above us only skies, Imagine all the people living for today"
+		},
+		"5": {
+			artist: "Pink Floyd",
+			songTitle: "Wish you were Here",
+			quote: "Running over the same old ground. <br> \
+				What have we found <br> \
+				The same old fears. <br> \
+				Wish you were here." 
+		},
+		"6": {
+			artist: "Simon & Garfunkel",
+			songTitle: "Sound of Silence",
+			quote: "People talking without speaking, people hearing without listening"
+		},
+		"7": {
+			artist: "Bob Dylan",
+			songTitle: "Tangled up in Blue",
+			quote: "Then she opened up a book of poems <br> \
+				And handed it to me <br> \
+				Written by an Italian poet <br> \
+				From the thirteenth century <br> \
+				And every one of them words rang true <br> \
+				And glowed like burning coal <br> \
+				Pouring off of every page <br> \
+				Like it was written in my soul from me to you <br> \
+				Tangled up in blue"
+		},
+		"8": {
+			artist: "Eminem",
+			songTitle: "Lose Yourself",
+			quote: "Look, if you had, one shot or one opportunity <br> \
+				To seize everything you ever wanted in one moment <br> \
+				Would you capture it, or just let it slip?" 
+		},
+		"9": {
+			artist: "Bob Dylan",
+			songTitle: "Like a Rolling Stone",
+			quote: "You said you’d never compromise <br> \
+				With the mystery tramp, but now you realize <br> \
+				He’s not selling any alibis <br> \
+				As you stare into the vacuum of his eyes <br> \
+				And ask him do you want to make a deal?"
+		},
+		"10": {
+			artist: "Avicii",
+			songTitle: "Wake Me Up",
+			quote: "I can't tell where the journey will end <br> \
+				But I know where to start"
+		},
+		"11": {
+			artist: "",
+			songTitle: "",
+			quote: ""
+		},
+		/*
+		: {
+			artist: "",
+			songTitle: "",
+			quote: ""
+		},
+		: {
+			artist: "",
+			songTitle: "",
+			quote: ""
+		},
+		: {
+			artist: "",
+			songTitle: "",
+			quote: ""
+		},
+		: {
+			artist: "",
+			songTitle: "",
+			quote: ""
+		},
+		: {
+			artist: "",
+			songTitle: "",
+			quote: ""
+		},
+		: {
+			artist: "",
+			songTitle: "",
+			quote: ""
+		},
+		: {
+			artist: "",
+			songTitle: "",
+			quote: ""
+		},
+		: {
+			artist: "",
+			songTitle: "",
+			quote: ""
+		},
+		: {
+			artist: "",
+			songTitle: "",
+			quote: ""
+		},
+		: {
+			artist: "",
+			songTitle: "",
+			quote: ""
+		},
+		: {
+			artist: "",
+			songTitle: "",
+			quote: ""
+		},
+		: {
+			artist: "",
+			songTitle: "",
+			quote: ""
+		},
+		: {
+			artist: "",
+			songTitle: "",
+			quote: ""
+		} */
+	}
+}
