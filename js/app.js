19adc1013021c6a06973a9b035922b4477828cd0
Aaron Cremona <dubbacremona@gmail.com>
2015-12-09 23:59:51 -0500

    split out css / js files

---
+++ js/app.js
@@ -0,0 +1,42 @@
+ function setNavPadding() {
+  var navHeight = $(".navbar").height();
+      //alert(navHeight);
+      $("body").css("margin-top", navHeight + "px");
+    }
+
+    function setSlideHeight() {
+      var maxSlideHeight = 0,
+      currHeight;
+      $("#myCarousel .item").each(function() {
+        currHeight = $(this).height();
+        if (currHeight > maxSlideHeight) {
+          maxSlideHeight = currHeight;
+        }
+        console.log("curr: " + currHeight + ", max: " + maxSlideHeight);
+      });
+
+      $("#myCarousel .carousel-content").each(function() {
+        $(this).height(maxSlideHeight + "px");
+      });
+    }
+
+    $(document).ready(function() {
+      setNavPadding();
+      setSlideHeight();
+    });
+
+    $(window).resize(function() {
+      setNavPadding();
+      setSlideHeight();
+    });
+
+    $("a[href^='#']").on('click', function(event) {
+      var target = this.hash,
+      navOffset = $('#navbar').height();
+
+      event.preventDefault();
+
+      return $('html, body').animate({
+        scrollTop: $(this.hash).offset().top - navOffset
+      }, 700);
+    });
